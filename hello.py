80dc14dd13371511552c9febe78f328207ac3e68
冯涛 <1508907793@qq.com>
2017-03-19 17:09:41 +0800

    设置Flask-WTF

--- hello.py
+++ hello.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
 
 manager = Manager(app)
 bootstap = Bootstrap(app)
@@ -18,9 +26,14 @@ def page_not_found(e):
 def internal_server_error(e):
 	return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow())
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
